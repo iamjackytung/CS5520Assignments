a06849a4eddb90aedb608ba63a9645fc97cfe8a1
Jacky Tung <jackyyet@usc.edu>
2023-02-01 00:32:29 -0800

    linear gradient created

--- Assignment1/App.js
+++ Assignment1/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions } from "react-native";
+let ScreenHeight = Dimensions.get("window").height;
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.top}></View>
+      <LinearGradient
+        colors={["rgb(140,205,242)", "rgb(118, 126, 181)"]}
+        style={styles.background}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: ScreenHeight,
+  },
+  top: {
+    margin: 10,
+    alignItems: "center",
+    width: "80%",
   },
 });
